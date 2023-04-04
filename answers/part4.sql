2ae11419df191db0dddb88c4a0176a580101cc7d
hiepnguyen90 <johnhiepnguyen1@gmail.com>
2023-04-03 23:38:20 -0400

    done

---
+++ answers/part4.sql
@@ -0,0 +1,8 @@
+SELECT pokemons.name, trainers.trainername, pokemon_trainer.pokelevel,
+a.name AS primary_type, b.name AS secondary_type
+FROM pokemons
+INNER JOIN types AS a ON primary_type = a.id
+INNER JOIN types AS b ON secondary_type = b.id
+INNER JOIN pokemon_trainer ON pokemon_trainer.pokemon_id = pokemons.id
+INNER JOIN trainers ON trainers.trainerID = pokemon_trainer.trainerID
+ORDER BY pokemon_trainer.pokelevel DESC
